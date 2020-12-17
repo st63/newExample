596b29634a6680a717310b80a18ce1ce853e4c97
Stas <st.sam017@gmail.com>
2020-12-17 15:27:41 +0400

    add drop-down lists

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-$(function(){
+$(function () {
 
+    $('button').click(function () {
+        $('.sidebar').toggleClass('size');
+    });
 
-$('button').click(function(){
-    $('.sidebar').toggleClass('size');
-});
-
+    $('.nav__link').click(function () {
+        $(this).parents('.nav__item').toggleClass('nav__item--open'); //обращаемся к родителю только текущего элемента
+    });
 
 });
