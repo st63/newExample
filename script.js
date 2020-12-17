f9912e18511b89bb29bb2cd9bf3dca6d996b4032
Stas <st.sam017@gmail.com>
2020-12-17 17:32:41 +0400

    toggle

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $(function () {
 
+    $('.nav__dropdown').hide();
+
     $('button').click(function () {
         $('.sidebar').toggleClass('size');
     });
 
     $('.nav__link').click(function () {
         $(this).parents('.nav__item').toggleClass('nav__item--open'); //обращаемся к родителю только текущего элемента
+        $(this).parents('.nav__item').children('.nav__dropdown').toggle(400, function(){
+            $(".nav__dropdown").not(this).hide(400);
+        });
     });
 
 });
