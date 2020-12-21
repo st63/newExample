f1bed4dbcf8c29654ce8ff42900a4558cef296e9
Stas <st.sam017@gmail.com>
2020-12-21 10:29:47 +0400

    droplists alerts & messages

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(function () {
 
     $('.nav__dropdown').hide();
     $('.user__dropdown').hide();
+    $('.alerts__dropdown').hide();
+    $('.message__dropdown').hide();
 
     $('.nav__link').click(function () {
         const parent = $(this).parent()
@@ -17,12 +19,25 @@ $(function () {
 
     $('.header__user--name').click(function () {
         $('.user__dropdown').toggle(400)
+        $('.alerts__dropdown').hide(400)
+        $('.message__dropdown').hide(400)
     });
 
     $('.btn').click(function () {
         $('.sidebar').toggleClass('size');
     });
+    
+    $('.header__user--alert').click(function () {
+        $('.alerts__dropdown').toggle(400)
+        $('.message__dropdown').hide(400)
+        $('.user__dropdown').hide(400)
+    });
 
+    $('.header__user--mail').click(function () {
+        $('.message__dropdown').toggle(400)
+        $('.alerts__dropdown').hide(400)
+        $('.user__dropdown').hide(400)
+    });
 
 
 });
