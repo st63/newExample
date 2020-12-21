6d2c7199f02bb9667f339f51cdcd9ba74a614b23
Stas <st.sam017@gmail.com>
2020-12-21 17:28:00 +0400

    click document & hide all dropdown

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
 $(function () {
-    const animationSpeed = 300
-
-    const navDropdown = $('.nav__dropdown')
-    const navLink = $('.nav__link')
-    const navItem = $('.nav__item')
+    const animationSpeed  = 300
+    const navDropdown     = $('.nav__dropdown')
+    const navLink         = $('.nav__link')
+    const navItem         = $('.nav__item')
+    const headerUserName  = $('.header__user--name')
+    const headerUserAlert = $('.header__user--alert')
+    const headerUserMail  = $('.header__user--mail')
+    const userDropdown    = $('.user__dropdown')
+    const messageDropdown = $('.message__dropdown')
+    const alertsDropdown  = $('.alerts__dropdown')
 
     navDropdown.hide();
-    $('.user__dropdown').hide();
-    $('.alerts__dropdown').hide();
-    $('.message__dropdown').hide();
+    userDropdown.hide();
+    alertsDropdown.hide();
+    messageDropdown.hide();
 
 
     navLink.click(function () {
         const parent = $(this).parent()
         const dropdown = parent.children('.nav__dropdown')
 
-        $('.nav__item').not(parent).removeClass('nav__item--open')
-        $('.nav__dropdown').not(dropdown).hide(animationSpeed)
+        navItem .not(parent).removeClass('nav__item--open')
+        navDropdown.not(dropdown).hide(animationSpeed)
 
         parent.toggleClass('nav__item--open')
         dropdown.toggle(animationSpeed)
     });
 
-    $('.header__user--name').click(function () {
-        $('.user__dropdown').toggle(animationSpeed)
-        $('.alerts__dropdown').hide(animationSpeed)
-        $('.message__dropdown').hide(animationSpeed)
+    headerUserName.click(function () {
+        userDropdown.toggle(animationSpeed)
     });
 
     $('.btn').click(function () {
         $('.sidebar').toggleClass('size');
     });
 
-    $('.header__user--alert').click(function () {
-        $('.alerts__dropdown').toggle(animationSpeed)
-        $('.message__dropdown').hide(animationSpeed)
-        $('.user__dropdown').hide(animationSpeed)
+    headerUserAlert.click(function () {
+        alertsDropdown.toggle(animationSpeed)
     });
 
-    $('.header__user--mail').click(function () {
-        $('.message__dropdown').toggle(animationSpeed)
-        $('.alerts__dropdown').hide(animationSpeed)
-        $('.user__dropdown').hide(animationSpeed)
+    headerUserMail.click(function () {
+        messageDropdown.toggle(animationSpeed)
     });
 
     $(document).click(function (e) {
@@ -49,6 +48,17 @@ $(function () {
             navDropdown.hide(animationSpeed);
         }
 
-        // продолжай тут
+        if (!headerUserName.is(e.target) && headerUserName.has(e.target).length === 0) {
+            userDropdown.hide(animationSpeed);
+        }
+
+        if (!headerUserMail.is(e.target) && headerUserMail.has(e.target).length === 0) {
+            messageDropdown.hide(animationSpeed);
+        }
+
+        if (!headerUserAlert.is(e.target) && headerUserAlert.has(e.target).length === 0) {
+            alertsDropdown.hide(animationSpeed);
+        }
+
     })
 });
