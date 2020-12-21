ab628db334520bbcce091082b1b519eb779a555d
renat <renat0014@gmail.com>
2020-12-21 12:39:10 +0400

    added example

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
 $(function () {
+    const animationSpeed = 300
 
-    $('.nav__dropdown').hide();
+    const navDropdown = $('.nav__dropdown')
+    const navLink = $('.nav__link')
+    const navItem = $('.nav__item')
+
+    navDropdown.hide();
     $('.user__dropdown').hide();
     $('.alerts__dropdown').hide();
     $('.message__dropdown').hide();
 
-    $('.nav__link').click(function () {
+
+    navLink.click(function () {
         const parent = $(this).parent()
         const dropdown = parent.children('.nav__dropdown')
 
         $('.nav__item').not(parent).removeClass('nav__item--open')
-        $('.nav__dropdown').not(dropdown).hide(400)
+        $('.nav__dropdown').not(dropdown).hide(animationSpeed)
 
         parent.toggleClass('nav__item--open')
-        dropdown.toggle(400)
-
+        dropdown.toggle(animationSpeed)
     });
 
     $('.header__user--name').click(function () {
-        $('.user__dropdown').toggle(400)
-        $('.alerts__dropdown').hide(400)
-        $('.message__dropdown').hide(400)
+        $('.user__dropdown').toggle(animationSpeed)
+        $('.alerts__dropdown').hide(animationSpeed)
+        $('.message__dropdown').hide(animationSpeed)
     });
 
     $('.btn').click(function () {
         $('.sidebar').toggleClass('size');
     });
-    
+
     $('.header__user--alert').click(function () {
-        $('.alerts__dropdown').toggle(400)
-        $('.message__dropdown').hide(400)
-        $('.user__dropdown').hide(400)
+        $('.alerts__dropdown').toggle(animationSpeed)
+        $('.message__dropdown').hide(animationSpeed)
+        $('.user__dropdown').hide(animationSpeed)
     });
 
     $('.header__user--mail').click(function () {
-        $('.message__dropdown').toggle(400)
-        $('.alerts__dropdown').hide(400)
-        $('.user__dropdown').hide(400)
+        $('.message__dropdown').toggle(animationSpeed)
+        $('.alerts__dropdown').hide(animationSpeed)
+        $('.user__dropdown').hide(animationSpeed)
     });
 
+    $(document).click(function (e) {
+        if (!navItem.is(e.target) && navItem.has(e.target).length === 0) {
+            navDropdown.hide(animationSpeed);
+        }
 
+        // продолжай тут
+    })
 });
