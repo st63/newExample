a3a854c685030659f64c820171d6d95800b42efc
renat <renat0014@gmail.com>
2020-12-24 21:14:42 +0400

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleancss     = require('gulp-clean-css');
 const browserSync  = require('browser-sync').create();
 
-function browsersync() {
+function browser() {
     browserSync.init({
         server: { baseDir: 'app/'},
         notify: false,
@@ -14,20 +14,20 @@ function browsersync() {
 }
 
 function styles() {
- return src('app/sсss/main.scss')
- .pipe(sass())
- .pipe(concat('app.min.css'))
- .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true }))
- .pipe(cleancss(({ level: { 1: { specialComments: 0 }}})))
- .pipe(dest('app/css/'))
- .pipe(browserSync.stream())
+    return src('app/sсss/main.scss')
+        .pipe(sass())
+        .pipe(concat('app.min.css'))
+        .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true }))
+        .pipe(cleancss(({ level: { 1: { specialComments: 0 }}})))
+        .pipe(dest('app/css/'))
+        .pipe(browserSync.stream())
 }
 
-function startwatch(){
+function startWatch(){
     watch('app/**/*.scss', styles);
     watch('app/**/*.html').on('change', browserSync.reload);
 }
 
-exports.browsersync = browsersync;
+exports.browserSync = browser;
 exports.styles  = styles;
-exports.default = parallel(styles, browsersync, startwatch);
+exports.default = parallel(browser, startWatch);
