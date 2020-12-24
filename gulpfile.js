a208047945a893ee631d0679da413d77d1b16cf1
Stas <st.sam017@gmail.com>
2020-12-24 20:43:10 +0400

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const concat       = require('gulp-concat');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss     = require('gulp-clean-css');
+const browserSync  = require('browser-sync').create();
+
+function browsersync() {
+    browserSync.init({
+        server: { baseDir: 'app/'},
+        notify: false,
+        online: true
+    })
+}
+
+function styles() {
+ return src('app/sсss/main.scss')
+ .pipe(sass())
+ .pipe(concat('app.min.css'))
+ .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true }))
+ .pipe(cleancss(({ level: { 1: { specialComments: 0 }}})))
+ .pipe(dest('app/css/'))
+ .pipe(browserSync.stream())
+}
+
+function startwatch(){
+    watch('app/**/*.scss', styles);
+    watch('app/**/*.html').on('change', browserSync.reload);
+}
+
+exports.browsersync = browsersync;
+exports.styles  = styles;
+exports.default = parallel(styles, browsersync, startwatch);
