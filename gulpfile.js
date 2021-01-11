37794346dd4eb940b0670ee50909a730d88022d3
Stas <st.sam017@gmail.com>
2021-01-11 19:12:35 +0400

    scss & scripts gulp1

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 const { src, dest, parallel, series, watch } = require('gulp');
-const concat                                 = require('gulp-concat');
-const sass                                   = require('gulp-sass');
-const autoprefixer                           = require('gulp-autoprefixer');
-const cleancss                               = require('gulp-clean-css');
-const browserSync                            = require('browser-sync').create();
-const posthtml                               = require('gulp-posthtml');
-const include                                = require('posthtml-include');
-const richtypo                               = require('posthtml-richtypo');
-const expressions                            = require('posthtml-expressions');
-const removeAttributes                       = require('posthtml-remove-attributes');
-const { quotes, sectionSigns, shortWords }   = require('richtypo-rules-ru');
-const imagemin                               = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const posthtml = require('gulp-posthtml');
+const include = require('posthtml-include');
+const richtypo = require('posthtml-richtypo');
+const expressions = require('posthtml-expressions');
+const removeAttributes = require('posthtml-remove-attributes');
+const { quotes, sectionSigns, shortWords } = require('richtypo-rules-ru');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify-es').default;
+
+function scripts() {
+  return src('src/script.js')
+    .pipe(uglify())
+    .pipe(dest('dist/'))
+    .pipe(browserSync.stream())
+}
 
 function imgProcess() {
   return src('src/img/*.svg')
@@ -24,21 +32,21 @@ function vendorCopy() {
 }
 
 function browser() {
-    browserSync.init({
-        server: { baseDir: 'dist/'},
-        notify: false,
-        online: true
-    })
+  browserSync.init({
+    server: { baseDir: 'dist/' },
+    notify: false,
+    online: true
+  })
 }
 
 function styles() {
-    return src('src/style.scss')
-        .pipe(sass())
-        .pipe(concat('app.min.css'))
-        .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true }))
-        .pipe(cleancss(({ level: { 1: { specialComments: 0 }}})))
-        .pipe(dest('dist/css/'))
-        .pipe(browserSync.stream())
+  return src('src/style.scss')
+    .pipe(sass())
+    .pipe(concat('app.min.css'))
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+    .pipe(cleancss(({ level: { 1: { specialComments: 0 } } })))
+    .pipe(dest('dist/css/'))
+    .pipe(browserSync.stream())
 }
 
 function htmlProcess() {
@@ -59,17 +67,17 @@ function htmlProcess() {
     .pipe(dest('dist'));
 }
 
-function startWatch(){
-    watch('src/**/*.scss', series(styles, browserSync.reload));
-    watch('src/pages/**/*.html', series(htmlProcess, browserSync.reload));
-    watch('src/components/**/*.html', series(htmlProcess, browserSync.reload));
-    watch('src/img/**/*.*', series(imgProcess, browserSync.reload));
-    watch('src/vendor/**/*.*', series(vendorCopy, browserSync.reload));
+function startWatch() {
+  watch('src/**/*.js', series(scripts, browserSync.reload));
+  watch('src/**/*.scss', series(styles, browserSync.reload));
+  watch('src/pages/**/*.html', series(htmlProcess, browserSync.reload));
+  watch('src/components/**/*.html', series(htmlProcess, browserSync.reload));
 }
 
 exports.browserSync = browser;
+exports.scripts = scripts;
 exports.styles = styles;
 exports.html = htmlProcess;
 exports.img = imgProcess;
 exports.vendor = vendorCopy;
-exports.start = parallel(styles, htmlProcess, imgProcess, vendorCopy, browser, startWatch);
+exports.start = parallel(styles, scripts, htmlProcess, imgProcess, vendorCopy, browser, startWatch);
